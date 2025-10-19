214b118a26380b3235a8d6b37f618decab938ca5
João Daniel De Cerqueira Lisboa <joaodan.lisboa@gmail.com>
2025-10-19 14:42:51 -0300

    Rename package for automoveisController

--- Estacionamento/Estacionamento/src/main/java/com/example/Estacionamento/Controller/automoveisController.java
+++ Estacionamento/Estacionamento/src/main/java/com/example/Estacionamento/Controller/automoveisController.java
@@ -1,4 +1,4 @@
-package com.example.Estacionamento.Controller;
+package com.example.Estacionamento2.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
