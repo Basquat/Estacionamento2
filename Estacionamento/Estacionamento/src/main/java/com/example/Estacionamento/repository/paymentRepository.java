39cfb9e7963e4cfd31a343784c43eb289aa3ce43
João Daniel De Cerqueira Lisboa <joaodan.lisboa@gmail.com>
2025-10-19 14:29:50 -0300

    Refactor paymentRepository for new package structure

--- Estacionamento/Estacionamento/src/main/java/com/example/Estacionamento/repository/paymentRepository.java
+++ Estacionamento/Estacionamento/src/main/java/com/example/Estacionamento/repository/paymentRepository.java
@@ -1,16 +1,18 @@
-package com.example.Estacionamento.repository;
+package com.Estacionamento2.repository;
 
-import com.example.Estacionamento.Entitys.paymentEntity;
+import com.estacionamento2.model.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface paymentRepository extends JpaRepository<paymentEntity, Long> {
-
-  paymentEntity findBypix(double pix);
-
-  paymentEntity findByDinheiro(double Dinheiro);
+public interface paymentRepository extends JpaRepository<Payment, Long> {
+    List<Payment> findByStatus(String status);
+    
+    @Query("SELECT SUM(p.valor) FROM Payment p WHERE p.status = 'pago'")
+    Double findTotalPaymentPago();
+    
+    List<Payment> findByAutomoveisId(Long automovelId);
 }
-
-//test
-//
