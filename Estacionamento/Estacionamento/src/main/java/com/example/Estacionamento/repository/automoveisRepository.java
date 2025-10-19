a9789dbcc318cc49dc84521a2f41ee1baa155a23
João Daniel De Cerqueira Lisboa <joaodan.lisboa@gmail.com>
2025-10-19 14:40:53 -0300

    Refactor automoveisRepository to use correct class name

--- Estacionamento/Estacionamento/src/main/java/com/example/Estacionamento/repository/automoveisRepository.java
+++ Estacionamento/Estacionamento/src/main/java/com/example/Estacionamento/repository/automoveisRepository.java
@@ -1,6 +1,6 @@
 package com.Estacionamento2.repository;
 
-import com.estacionamento2.Entitys.automoveis;
+import com.Estacionamento2.Entitys.automoveis;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -9,21 +9,21 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface automoveisRepository extends JpaRepository<Automoveis, Long> {
-    Optional<Automoveis> findByPlaca(String placa);
-    List<Automoveis> findByType(String type);
-    List<Automoveis> findByPago(Boolean pago);
+public interface automoveisRepository extends JpaRepository<automoveis, Long> {
+    Optional<automoveis> findByPlaca(String placa);
+    List<automoveis> findByType(String type);
+    List<automoveis> findByPago(Boolean pago);
     boolean existsByPlaca(String placa);
     
-    @Query("SELECT COUNT(a), SUM(a.valor) FROM Automoveis a WHERE a.pago = true AND a.formaPagamento = 'PIX'")
+    @Query("SELECT COUNT(a), SUM(a.valor) FROM automoveis a WHERE a.pago = true AND a.formaPagamento = 'PIX'")
     Object[] getTotalPayment();
     
-    @Query("SELECT COUNT(a), SUM(a.valor) FROM Automoveis a WHERE a.pago = true AND a.formaPagamento = 'DINHEIRO'")
+    @Query("SELECT COUNT(a), SUM(a.valor) FROM automoveis a WHERE a.pago = true AND a.formaPagamento = 'DINHEIRO'")
     Object[] getTotalDinheiro();
     
-    @Query("SELECT a.type, COUNT(a), SUM(a.valor) FROM Automoveis a WHERE a.pago = true GROUP BY a.type")
+    @Query("SELECT a.type, COUNT(a), SUM(a.valor) FROM automoveis a WHERE a.pago = true GROUP BY a.type")
     List<Object[]> getTotaisPorTipoPagos();
     
-    @Query("SELECT a.type, COUNT(a), SUM(a.valor) FROM Automoveis a WHERE a.pago = false GROUP BY a.type")
+    @Query("SELECT a.type, COUNT(a), SUM(a.valor) FROM automoveis a WHERE a.pago = false GROUP BY a.type")
     List<Object[]> getTotaisPorTipoPendentes();
 }
