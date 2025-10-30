c4040972bd31a78197820ebe072639ad81192a79
Basquat <joaodan.lisboa@gmail.com>
2025-10-29 23:11:44 -0300

    testing pom.xml

--- Estacionamento/Estacionamento/src/main/java/com/example/Estacionamento/Application.java
+++ Estacionamento/Estacionamento/src/main/java/com/example/Estacionamento/Application.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Estacionamento2Application.class, args);
+       SpringApplication.run(Application.class, args);
     }
     
     @Bean
